543050209232d43583aeb3569437cee1279a76d9
irfanakbari <irfanakbarihabibi18@gmail.com>
2024-01-28 19:23:41 +0700

    refactor

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -6,10 +6,10 @@ export async function GET() {
 
   if (session) {
 
-    const idToken = session.id_token
+    // const idToken = session.id_token
 
     // this will log out the user on Keycloak side
-    const url = `${process.env.KEYCLOACK_END_URL}?id_token_hint=${idToken}&post_logout_redirect_uri=${encodeURIComponent(process.env.NEXTAUTH_URL!)}`;
+    const url = `${process.env.KEYCLOACK_END_URL}?post_logout_redirect_uri=${encodeURIComponent(process.env.NEXTAUTH_URL!)}`;
 
     try {
       await fetch(url, { method: "GET" });
